e2621fce1ea3fd7f423e6c13c3222668f90ffcd7
coki28 <149650290+coki28@users.noreply.github.com>
2023-11-02 16:36:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Student of Citcs", "Scorpio","Music Lover","Foodie"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
